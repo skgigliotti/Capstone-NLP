21248cebe7312a9c3848f215875f1e74f784c922
Sophia <sgigliotti@westmont.edu>
2020-05-01 13:45:01 -0700

    processing class funcitoning

--- project/translationcapstone/userinterface/models.py
+++ project/translationcapstone/userinterface/models.py
@@ -11,4 +11,10 @@ class Post(models.Model):
 
 class Translation(models.Model):
     orginal = models.CharField(max_length=500)
+    polarity_original = models.FloatField()
+    subjectivity_original = models.FloatField()
+    polarity_google = models.FloatField()
+    subjectivity_google = models.FloatField()
     target = models.CharField(max_length=500)
+    polarity_target = models.FloatField()
+    subjectivity_target = models.FloatField()
--- project/translationcapstone/userinterface/views.py
+++ project/translationcapstone/userinterface/views.py
@@ -4,6 +4,7 @@ from .forms import TextForm
 from .models import Question
 from .models import Post
 from .markov import predictions
+from .sentiment import processingsingular
 
 def results(request, question_id):
     response = "Here are the questions %s"
@@ -21,12 +22,23 @@ def index(request):
 
     return render(request, 'index.html', context)
 
-def text_view(request):
+def markov_view(request):
     if request.method == "POST":
+        #get the different information from the form
+        option1 = request.POST.get('option1')
+        option2 = request.POST.get('option2')
+        original = request.POST.get('original')
+        target = request.POST.get('target')
 
-        texts = [request.POST.get('option1'),request.POST.get('option2')]
-        #Question.objects.create(question_text=text)
-        probabilities = predictions.train(texts)
-        
+        #check for what has actually been filled in
+        if ((option1 != '') and (option2 != '')):
+            texts = [option1,option2]
+            probabilities = predictions.train(texts)
+            print(probabilities)
+        if (( original != '') and ( target != '' )):
+            texts = [original,target]
+            print(texts)
+            processingsingular.analyze(texts)
+            #Translation.objects.create(original=original,target=target)
     context = {}
     return render(request, "enter_options.html",context)
