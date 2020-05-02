553816073d8540b244bbc7db9c8a8885f9445e8e
Sophia <sgigliotti@westmont.edu>
2020-05-01 19:49:11 -0700

    preparing app for heroku

--- project/translationcapstone/translationtoolcapstone/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TranslationtoolcapstoneConfig(AppConfig):
-    name = 'translationtoolcapstone'
--- project/translationcapstone/userinterface/urls.py
+++ project/translationcapstone/userinterface/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/detail', views.detail, name='detail'),
-    path('form/', views.markov_view, name='form'),
     path('results/<int:question_id>/', views.results, name='results'),
     path('results/index/',views.index, name='index'),
+    path('form/', views.markov_view, name='form'),
 ]
